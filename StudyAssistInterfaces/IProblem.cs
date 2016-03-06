2d66b4a7cf3ca1b6b064e959f5df4c6a123875c1
FruityBrew <kompota@yandex.ru>
2016-03-06 21:38:21 +0300

    ModelInterfacesAdded

---
+++ StudyAssistInterfaces/IProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudyAssistInterfaces
+{
+    public interface IProblem
+    {
+        String Question { get; set; }
+        String Answer { get; set; }
+
+        Byte Study { get; set; }
+
+        Boolean IsStudy { get; set; }
+    }
+}
